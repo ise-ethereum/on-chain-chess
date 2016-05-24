42fc20a38349fac5122d1ac7971a3627e40553e2
Dennis.westphal <mail@denniswestphal.de>
2016-05-24 20:11:02 +0200

    Display play game page

--- app/javascript/playGame.js
+++ app/javascript/playGame.js
@@ -1,4 +1,28 @@
-/* global angular */
-angular.module('dappChess').controller('PlayGameCtrl', function () {
-
+/* global angular, inArray */
+angular.module('dappChess').controller('PlayGameCtrl', function (games, $route, $scope) {
+  function checkOpenGame(gameId) {
+    return inArray(gameId, games.openGames);
+  }
+  
+  $scope.getGameId = function() {
+    return $route.current.params.id;
+  };
+  $scope.isOpenGame = function() {
+    let gameId = $scope.getGameId();
+    
+    if(gameId) {
+      return checkOpenGame(gameId);
+    }
+    
+    return false;
+  };
+  $scope.getGame = function() {
+    let gameId = $scope.getGameId();
+    
+    if(gameId) {
+      return games.getGame(gameId);
+    }
+    
+    return false;
+  };
 });
