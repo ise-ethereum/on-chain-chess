d90a72937bba4e34a9638ed8630126ec24a93f4b
Paul Grau <paul@productgang.com>
2016-05-26 22:43:58 +0200

    Add default board state and basics for move() function. #38

--- test/test_contract.js
+++ test/test_contract.js
@@ -3,6 +3,15 @@ import { Chess, web3 } from '../contract/Chess.sol';
 
 var assert = require('chai').assert;
 
+const defaultBoard = [-4,-2,-3,-5,-6,-3,-2,-4,0,0,0,0,0,0,0,0,
+                      -1,-1,-1,-1,-1,-1,-1,-1,0,0,0,0,0,0,0,0,
+                      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                      0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+                      1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,
+                      4,2,3,5,6,3,2,4,0,0,0,0,0,0,0,0];
+
 describe('Chess contract', function() {
   this.timeout(10000);
   this.slow(500);
@@ -14,7 +23,7 @@ describe('Chess contract', function() {
   // We create a few test games here that will later be accessed in testGames[]
   describe('initGame()', function () {
     it('should initialize a game with player1 playing white', function (done) {
-      Chess.initGame('Alice', true, {from: player1, gas: 1000000});
+      Chess.initGame('Alice', true, {from: player1, gas: 2000000});
 
       // Watch for event from contract to check if it worked
       var filter = Chess.GameInitialized({});
@@ -29,8 +38,20 @@ describe('Chess contract', function() {
       });
     });
 
+    it('should broadcast the initial game state', function(done) {
+      Chess.initGame('Bob', true, {from: player1, gas: 2000000});
+
+      var eventGamestate = Chess.GameStateChanged({});
+      eventGamestate.watch(function(error, result){
+        let state = result.args.state.map(n => n.toNumber());
+        assert.deepEqual(defaultBoard, state);
+        eventGamestate.stopWatching(); // Need to remove filter again
+        done();
+      });
+    });
+
     it('should initialize a game with player1 playing black', function (done) {
-      Chess.initGame('Susan', false, {from: player1, gas: 1000000});
+      Chess.initGame('Susan', false, {from: player1, gas: 2000000});
 
       // Watch for event from contract to check if it worked
       var filter = Chess.GameInitialized({});
@@ -90,29 +111,38 @@ describe('Chess contract', function() {
   describe('move()', function () {
     it('should throw an exception when it is not this player\'s turn', function() {
       assert.throws(function(){
-        Chess.move(testGames[0], {from: player2, gas: 100000});
+        Chess.move(testGames[0], 0, 0, {from: player2, gas: 100000});
       }, Error);
     });
 
     it('should throw an exception when player with color black starts', function() {
       assert.throws(function(){
-        Chess.move(testGames[1], {from: player1, gas: 100000});
+        Chess.move(testGames[1], 0, 0, {from: player1, gas: 100000});
       }, Error);
     });
 
     it('should accept a valid move', function(done) {
       // As player1 is the next player, this move should be valid
       assert.doesNotThrow(function(){
-        Chess.move(testGames[0], {from: player1, gas: 100000});
+        Chess.move(testGames[0], 99, 83, {from: player1, gas: 100000});
       }, Error);
 
       // Watch for event from contract to check if it worked
       var filter = Chess.Move({});
       filter.watch(function(error, result){
-        assert.isTrue(result.args.moveSuccesful);
+        assert.equal(player1, result.args.player);
+        assert.equal(99, result.args.fromIndex);
+        assert.equal(83, result.args.toIndex);
         filter.stopWatching(); // Need to remove filter again
         done();
       });
+    });
+
+    it('should have updated nextPlayer after the previous move', function() {
+      assert.throws(function(){
+        // Cannot move again from player1 because nextPlayer will be player2
+        Chess.move(testGames[0], 0, 0, {from: player1, gas: 100000});
+      }, Error);
     });
   });
 
