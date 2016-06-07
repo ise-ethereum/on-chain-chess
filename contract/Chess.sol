9485133549b5561bf259bfaf1c5cb4b42582dc07
lisasi <erle@campus.tu-berlin.de>
2016-06-08 00:50:28 +0200

    Fix figure comparison in ifs

--- contract/Chess.sol
+++ contract/Chess.sol
@@ -292,8 +292,8 @@
             abs(fromFigure) == uint(Pieces(Piece.WHITE_ROOK))) {
 
             // Bishop can only walk diagonally, Rook only non-diagonally
-            if (!isDiagonal && abs(fromFigure) == uint(Pieces(Piece.BLACK_BISHOP)) ||
-                isDiagonal && abs(fromFigure) == uint(Pieces(Piece.BLACK_ROOK))) {
+            if (!isDiagonal && abs(fromFigure) == uint(Pieces(Piece.WHITE_BISHOP)) ||
+                isDiagonal && abs(fromFigure) == uint(Pieces(Piece.WHITE_ROOK))) {
                 return false;
             }
 
@@ -565,7 +565,7 @@
         int8 direction = getDirection(fromIndex, toIndex);
 
         // PAWN - EN PASSANT or DOUBLE STEP
-        if (abs(fromFigure) == uint(Pieces(Piece.BLACK_PAWN))) {
+        if (abs(fromFigure) == uint(Pieces(Piece.WHITE_PAWN))) {
             // En Passant - remove caught pawn
             // en passant if figure: pawn and diagonal move to empty field
             if (is_diagonal(direction) && toFigure == Pieces(Piece.EMPTY)) {
@@ -606,7 +606,7 @@
     // checks whether movingPlayerColor's king gets checked by move
     function checkLegality(bytes32 gameId, uint256 fromIndex, uint256 toIndex, int8 fromFigure, int8 toFigure, int8 movingPlayerColor) internal returns (bool){
         // the king already gets tested when he moves
-        if (abs(fromFigure) == uint(Pieces(Piece.BLACK_KING)))
+        if (abs(fromFigure) == uint(Pieces(Piece.WHITE_KING)))
             return;
 
         // through move of fromFigure from fromIndex king may now be in danger from that direction
