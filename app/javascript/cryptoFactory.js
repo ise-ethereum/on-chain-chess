aa2e165ab780c2151edc8c0d9a463895711fdf03
Kyroy <info@kyroy.com>
2016-06-23 11:49:09 +0200

    Testing crypto for Client

---
+++ app/javascript/cryptoFactory.js
@@ -0,0 +1,45 @@
+/* global angular */
+var ethUtils = require('../../node_modules/ethereumjs-util/index');
+
+import {web3} from '../../contract/Chess.sol';
+angular.module('dappChess').factory('crypto', function () {
+  let crypto = {};
+  console.log('crypto loaded');
+
+  /**
+   * Calculates the signature of the given data.
+   * @param{string} account to be used for signing
+   * @param{object} data to be signed
+   * @returns {string} the signature of the given data
+   */
+  crypto.sign = function (account, data) {
+    let hash = web3.sha3(data);
+    return web3.eth.sign(account, hash);
+  };
+
+  crypto.verify = function (account, signature, data) {
+    let msgHash = web3.sha3(data);
+    let r = signature.slice(0, 66);
+    let s = '0x' + signature.slice(66, 130);
+    let v = '0x' + signature.slice(130, 132);
+    v = web3.toDecimal(v);
+
+    console.log('r', r);
+    console.log('s', s);
+    console.log('v', v);
+    console.log('msgHash', msgHash);
+
+    let signerAddress = ethUtils.ecrecover(new Buffer(msgHash, 'hex'), v, r, s);
+    console.log('signerAddress', signerAddress.toString('hex'));
+    console.log('account', account.slice(2));
+    return account === signerAddress.toString('hex');
+  };
+
+  let text = 'My super text to be signed';
+  let signature = crypto.sign(web3.eth.accounts[0], text);
+  console.log('signature', signature);
+
+  console.log('verify', crypto.verify(web3.eth.accounts[0], signature, text));
+
+  return crypto;
+});
--- app/javascript/welcome.js
+++ app/javascript/welcome.js
@@ -1,3 +1,3 @@
 /* global angular */
-angular.module('dappChess').controller('WelcomeCtrl', function () {
+angular.module('dappChess').controller('WelcomeCtrl', function (crypto) {
 });
