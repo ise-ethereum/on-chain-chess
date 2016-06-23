106f79a00f46d58a73ab0fdc16bf56fbd1bed4cc
Kyroy <info@kyroy.com>
2016-06-23 16:15:53 +0200

    Change implicit return to return (Auth)

--- contract/Auth.sol
+++ contract/Auth.sol
@@ -1,6 +1,6 @@
 contract Auth {
-    function getAddress(bytes32 hash, uint8 v, bytes32 r, bytes32 s) constant returns(address retAddr) {
-        retAddr = ecrecover(hash, v, r, s);
+    function getAddress(bytes32 hash, uint8 v, bytes32 r, bytes32 s) constant returns(address) {
+        return ecrecover(hash, v, r, s);
     }
 
     function verify(address account, bytes32 hash, uint8 v, bytes32 r, bytes32 s) constant returns(bool) {
