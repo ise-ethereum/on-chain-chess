85d654aa45ebb38181bcbd36dd3a5737c9cb87af
Dennis.westphal <mail@denniswestphal.de>
2016-06-28 10:48:06 +0200

    Merge & add player winnings

---
+++ contract/Auth.sol
@@ -0,0 +1,9 @@
+contract Auth {
+    function getAddress(bytes32 hash, uint8 v, bytes32 r, bytes32 s) constant returns(address) {
+        return ecrecover(hash, v, r, s);
+    }
+
+    function verify(address account, bytes32 hash, uint8 v, bytes32 r, bytes32 s) constant returns(bool) {
+        return account == getAddress(hash, v, r, s);
+    }
+}
