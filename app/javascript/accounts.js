6f78164007d92960f8fda2300d86cfe634fcafaf
Dennis.westphal <mail@denniswestphal.de>
2016-06-30 09:07:22 +0200

    Move account selection logic to accounts.js and add controller for layer

--- app/javascript/accounts.js
+++ app/javascript/accounts.js
@@ -1,25 +1,21 @@
 /* global angular, mist, blockies */
 import {web3} from '../../contract/Chess.sol';
 angular.module('dappChess').factory('accounts', function () {
-  return {
+  let accounts = {
     // Use the first 5 accounts for mist or firefox and the last 5 for other browsers
     availableAccounts: (typeof(mist) !== 'undefined' ||
     window.navigator.userAgent.indexOf('Firefox') !== -1) ?
       web3.eth.accounts.slice(0, Math.floor(web3.eth.accounts.length / 2)) :
       web3.eth.accounts.slice(Math.floor(web3.eth.accounts.length / 2), web3.eth.accounts.length),
     defaultAccount: web3.eth.defaultAccount,
-    requestAccount: (typeof(mist) !== 'undefined') ?
-      mist.requestAccount :
-      function(callback) {
-        console.log('Not implemented yet');
-        callback();
-      },
+    selectedAccount: null,
+    selectedAccountName: null,
     // Get ether balance with 4 digit precision
     getBalance: function(account) {
       if(web3.eth.accounts.indexOf(account) !== -1) {
         return web3.fromWei(
-            web3.eth.getBalance(account), 'ether'
-          ).toDigits(20, 3).toString(10);
+          web3.eth.getBalance(account), 'ether'
+        ).toDigits(20, 3).toString(10);
       }
 
       return false;
@@ -37,4 +33,48 @@ angular.module('dappChess').factory('accounts', function () {
       }
     }
   };
+  accounts.isSelectedAccount = function(account) {
+    return accounts.selectedAccount === account;
+  };
+  accounts.setSelectedAccount = function(account, name) {
+    accounts.selectedAccount = account;
+    if (name) {
+      accounts.selectedAccountName = name;
+    }
+    else {
+      accounts.selectedAccountName = accounts.selectedAccount.substr(0, 7) +
+        '...' + accounts.selectedAccount.substr(-7, 7);
+    }
+  };
+  accounts.selectOrCreateAccount = function($event) {
+    $event.preventDefault();
+
+    if(typeof(mist) !== 'undefined') {
+      accounts.setSelectedAccount(mist.requestAccount());
+    }
+    else {
+      jQuery('#overlay').show();
+      jQuery('#selectAccountLayer').show();
+    }
+  };
+  accounts.selectAccount = function(account, name) {
+    if(name) {
+      accounts.setSelectedAccount(account, name);
+    }
+    else {
+      accounts.setSelectedAccount(account);
+    }
+    $('#selectAccountLayer').fadeOut();
+    $('#overlay').fadeOut();
+  };
+  return accounts;
+}).controller('accountsCtrl', function(accounts, $scope) {
+  $scope.accounts = accounts;
+
+  jQuery(document).ready(function($) {
+    $('#overlay').click(function() {
+      $('#layers > .layer').fadeOut();
+      $('#overlay').fadeOut();
+    });
+  });
 });
--- app/javascript/initializeGame.js
+++ app/javascript/initializeGame.js
@@ -2,15 +2,11 @@
 import {web3, Chess} from '../../contract/Chess.sol';
 angular.module('dappChess').controller('InitializeGameCtrl',
   function ($rootScope, $scope, accounts) {
-    $scope.availableAccounts = accounts.availableAccounts;
-    $scope.selectedAccount = accounts.defaultAccount;
     $scope.startcolor = 'white';
     $scope.username = null;
     $scope.etherbet = 0;
 
-    $scope.isSelectedAccount = function (account) {
-      return $scope.selectedAccount === account;
-    };
+    $scope.accounts = accounts;
 
     function initializeGame() {
       $rootScope.$broadcast('message', 'Your game is being created, please wait a moment...',
@@ -33,20 +29,9 @@ angular.module('dappChess').controller('InitializeGameCtrl',
       }
     }
 
-    $scope.selectOrCreateAccount = function($event) {
-      $event.preventDefault();
-
-      accounts.requestAccount(function(e, address) {
-        $scope.selectedAccount = address;
-      });
-    };
-
     $scope.initializeGame = function (form) {
       if(form.$valid) {
         initializeGame();
       }
     };
-
-    $scope.getBalance = accounts.getBalance;
-    $scope.getBlockie = accounts.getBlockie;
   });
